b79d19f66e979f9730ff7a3602403ddfaab767f7
Annie Tran <132701766+AnnieTran2023@users.noreply.github.com>
2024-01-19 23:33:34 +0200

    Update pyramid.py

--- chapter-3/pyramid.py
+++ chapter-3/pyramid.py
@@ -1,1 +1,15 @@
+def print_pyramid(height):
+    for i in range(1, height + 1):
+        # Print spaces
+        for j in range(height - i):
+            print(" ", end="")
+        # Print asterisks
+        for k in range(2 * i - 1):
+            print("*", end="")
+        print()
 
+def main():
+	height = int(input("Enter pyramid height: "))
+	print_pyramid(height)
+
+main()
