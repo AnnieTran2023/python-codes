c2925434ab0f50be2a820c361a2f196af0d74432
Annie Tran <tranthilananh105@gmail.com>
2024-06-03 22:44:28 +0300

    coffee mavhine OOP updated

--- day-16/oop-coffee-machine/main.py
+++ day-16/oop-coffee-machine/main.py
@@ -2,14 +2,22 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-user_choice = input("What would you like? (espresso/latte/cappuccino/): ")
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-while user_choice != "off":
-    if user_choice == "report":
-        menu = MenuItem()
-        report = report()
-        print(report)
-    elif user_choice not in menu:
-        print(f"We do not serve {user_choice} in this machine. Please choose again!")    
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
     else:
+        drink = menu.find_drink(choice)
         
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+          coffee_maker.make_coffee(drink)
