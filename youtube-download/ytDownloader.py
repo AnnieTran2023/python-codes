4621245e9d3513df4ba50a4a457c1405a8a33e15
Annie Tran <tranthilananh105@gmail.com>
2024-06-05 23:31:47 +0300

    edit ytDownloader

--- youtube-download/ytDownloader.py
+++ youtube-download/ytDownloader.py
@@ -1,7 +1,18 @@
 from pytube import YouTube
 from sys import argv
 
+if len(argv) < 2:
+    print("Usage: python ytDownloader.py <YouTube URL>")
+    exit(1)
+
 link = argv[1]
 yt = YouTube(link)
 
-print(yt.title)
+print("Title:", yt.title)
+print("Views:", yt.views)
+print("Length:", yt.length, "seconds")
+print("Rating:", yt.rating)
+
+video = yt.streams.get_highest_resolution()
+
+video.download("/Users/annietran/Desktop")
